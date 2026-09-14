77b18abc0123c22713510a9290cf792e0f7d79ee
judithbuck <judith18buckden@hotmail.com>
2026-09-14 22:10:03 +0100

    comments updated

--- FunctionPower/Program.cs
+++ FunctionPower/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             // complete the code below for a function 'Power' to calculate
-            // result of 7 ^ 2
+            // result of 7 ^ 2 by replacing the '?' with the appropriate C# code
             // test with other 'power' calculations eg 8 ^ 3,  5 ^ 1, 15 ^ 0
             int myresult = Power();
             Console.WriteLine("7 to the power 2  = " + myresult);
@@ -15,6 +15,7 @@
         {
             int num = 7;
             int power = 2;
+
             int result = ?;
 
             for (int i = ?; i <= ?; i++)
