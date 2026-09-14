aaf928b906d6cbf7dc1901ada611e5385834fc1f
judithbuck <judith18buckden@hotmail.com>
2026-09-14 21:56:40 +0100

    Add project files.

---
+++ FunctionPower/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace FunctionPower
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // complete the code below for a function 'Power' to calculate
+            // result of 7 ^ 2
+            // test with other 'power' calculations eg 8 ^ 3,  5 ^ 1, 15 ^ 0
+            int myresult = Power();
+            Console.WriteLine("7 to the power 2  = " + myresult);
+        }
+
+        static int Power()
+        {
+            int num = 7;
+            int power = 2;
+            int result = ?;
+
+            for (int i = ?; i <= ?; i++)
+            {
+                result = ?;
+            }
+            return result;
+        }
+
+    }
+}
